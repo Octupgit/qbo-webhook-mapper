9f6fcc2f7651e76493beced58e4dbab3e00ea674
Moshe Gold <moshe@octup.com>
2026-02-10 15:11:35 +0200

    remove utils

--- app/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.utils.crypto import encrypt_token, decrypt_token
-from app.utils.oauth_state import generate_state, verify_state
-
-__all__ = ["encrypt_token", "decrypt_token", "generate_state", "verify_state"]
