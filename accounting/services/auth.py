10e9eb3d97338a0f661b36f1c7302b9a0fe53cf7
Moshe Gold <moshe@octup.com>
2026-02-12 13:26:26 +0200

    callback in auth service

--- accounting/services/auth.py
+++ accounting/services/auth.py
@@ -1,5 +1,7 @@
 from accounting.strategies import AccountingSystemFactory
-from accounting.utils.oauth_state import generate_state
+from accounting.utils.oauth_state import generate_state, verify_state
+from starlette.requests import Request
+from accounting.models import BaseAuthResult
 
 
 class AuthService:
@@ -12,3 +14,22 @@ class AuthService:
         auth_url = await strategy.get_authorization_url(partner_id=partner_id, state=state)
 
         return auth_url
+
+    async def handle_callback(self, request: Request) -> BaseAuthResult:
+        state = request.query_params.get("state")
+
+        if not state:
+            raise ValueError("Missing state parameter")
+
+        verified_state = verify_state(state)
+
+        if verified_state is None:
+            raise ValueError("Invalid or expired state")
+
+        accounting_system = verified_state["accounting_system"]
+
+        strategy = AccountingSystemFactory.get_strategy(accounting_system)
+
+        auth_result = await strategy.handle_callback(request=request)
+
+        return auth_result
